4a5f7068984ef757ed86088695fe37b0ddfa719a
henriqueedu2001 <henrique_eduardo_souza@hotmail.com>
2026-06-15 14:33:11 -0300

    feat: add building endpoints to router

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.routes import health_check, auth, user, institution
+from app.api.routes import health_check, auth, user, institution, building
 
 app = FastAPI()
 
@@ -7,3 +7,4 @@ app.include_router(health_check.router)
 app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(institution.router)
+app.include_router(building.router)
