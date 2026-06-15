55f9bf428165eb6f039e69ba5867b8029dbdbce9
henriqueedu2001 <henrique_eduardo_souza@hotmail.com>
2026-06-15 10:52:32 -0300

    add: bearer token auth

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from app.schemas.auth_models import *
 from app.database.database_manager import *
 from app.database.repositories import *
@@ -6,6 +7,19 @@ from app.modules.auth.jwt_token import *
 from http import HTTPStatus
 
 router = APIRouter()
+
+security = HTTPBearer()
+
+def verify_token(credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)]):
+    token = credentials.credentials
+    
+    if not validate_jwt_token(token):
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail="invalid or expired token",
+        )
+    return token
+
 
 @router.post('/auth/user')
 def auth_user(user_credentials: AuthUserRequest, db: Database = Depends(get_database)) -> AuthUserResponse:
@@ -29,7 +43,13 @@ def auth_user(user_credentials: AuthUserRequest, db: Database = Depends(get_data
     else:
         raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED)
 
+
 @router.post('/auth/token')
 def auth_token(jwt_token: str) -> bool:
     valid_token = validate_jwt_token(jwt_token)
     return valid_token
+
+
+@router.get('/auth/request')
+def auth_request(token: Annotated[str, Depends(verify_token)]):
+    return {"message": "Access granted!", "token_used": token}
