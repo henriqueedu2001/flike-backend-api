3f5215bce62dc71991416aeec7b2d9beff854334
henriqueedu2001 <henrique_eduardo_souza@hotmail.com>
2026-06-30 10:51:06 -0300

    chore: typo

--- app/api/routes/institution.py
+++ app/api/routes/institution.py
@@ -14,11 +14,11 @@ def get_all_institution(db: Database = Depends(get_database)):
 
 
 @router.post('/institution/new')
-def create_institution(user_id: int, building_name: str, db: Database = Depends(get_database)):
+def create_institution(user_id: int, institution_name: str, db: Database = Depends(get_database)):
     repo = InstitutionRepository(db)
 
     try:
-        institution_id, created_at = repo.create_institution(user_id, building_name)
+        institution_id, created_at = repo.create_institution(user_id, institution_name)
         return CreateInstitutionResponse(institution_id=institution_id, created_at=created_at)
     except Exception as error:
         raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=str(error))
